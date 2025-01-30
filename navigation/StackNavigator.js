5291cf5e84d2e31648ea21d87203c1d0a8363bb6
fatdog <fatdog@fatdogs-MacBook-Pro.local>
2025-01-30 21:24:08 +0545

    layout

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,97 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import PlayScreen from '../screens/PlayScreen';
+import BookScreen from '../screens/BookScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  function BottomTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HOME"
+          component={HomeScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <Ionicons name="home-outline" size={24} color="green" />
+              ) : (
+                <Ionicons name="home-outline" size={24} color="#989898" />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="PLAY"
+          component={PlayScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <Ionicons name="home-outline" size={24} color="green" />
+              ) : (
+                <Ionicons name="home-outline" size={24} color="#989898" />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="BOOK"
+          component={BookScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <Ionicons name="home-outline" size={24} color="green" />
+              ) : (
+                <Ionicons name="home-outline" size={24} color="#989898" />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="PROFILE"
+          component={ProfileScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <Ionicons name="home-outline" size={24} color="green" />
+              ) : (
+                <Ionicons name="home-outline" size={24} color="#989898" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
+  const AuthStack = () => {
+    return <Stack.Navigator></Stack.Navigator>;
+  };
+
+  function MainStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    );
+  }
+  return (
+    <NavigationContainer>
+        <MainStack />
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
