f746b4a244dea32f824f2e36d4f987d9299d9496
fatdog <fatdog@fatdogs-MacBook-Pro.local>
2025-01-31 21:34:33 +0545

    changes

--- screens/PlayScreen.js
+++ screens/PlayScreen.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const PlayScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>PlayScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
